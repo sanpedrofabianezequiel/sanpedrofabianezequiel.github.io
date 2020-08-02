4e9711dc32f403415b379f52871956067ef45fed
ezequiel <ezequiel@ezequiel.com>
2020-08-02 17:38:23 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var idLinkedin= document.querySelector("#idLinkedin")
+idLinkedin.addEventListener('click',(e) => { location.href= "https://www.linkedin.com/in/ezequiel-san-pedro-24439b17b/" })
+
+var urlAppWeb= document.querySelector("#urlAppWeb")
+urlAppWeb.addEventListener('click',(e) => { location.href= "https://github.com/sanpedrofabianezequiel/Biblioteca-App-Web"})
+
+var idAndroid= document.querySelector("#idAndroid")
+idAndroid.addEventListener('click',(e) => { location.href= "https://github.com/sanpedrofabianezequiel/Beer-shop"})
+
+
+var idIconos= document.querySelectorAll(".sPequeña");
+
+idIconos.forEach(   x => {
+    x.addEventListener('click', function () {  location.href= "https://www.linkedin.com/in/ezequiel-san-pedro-24439b17b/" }) 
+});
+
+var urlPong=document.querySelector('#urlPong')
+
+urlPong.addEventListener('click',function () {
+    location.href="https://github.com/sanpedrofabianezequiel/Pong.git"
+  })
+
