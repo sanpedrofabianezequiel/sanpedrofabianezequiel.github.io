66a89471d4b3d7cb2a747af68f866c78c871e9fc
ezequiel <ezequiel@ezequiel.com>
2020-08-10 15:56:09 -0300

    Update Ajax/Rest

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ idAndroid.addEventListener('click',(e) => { location.href= "https://github.com/s
 var idJavaProcesador= document.querySelector("#idJavaProcesador")
 idJavaProcesador.addEventListener('click',(e) => { location.href= "https://github.com/sanpedrofabianezequiel/ProcesadorDeTextos"})
 
+var idJavaProcesador= document.querySelector("#idAjaxRest")
+idJavaProcesador.addEventListener('click',(e) => { location.href= "https://github.com/sanpedrofabianezequiel/AjaxREST"})
+
+
+
 var idIconos= document.querySelectorAll(".sPequeña");
 
 idIconos.forEach(   x => {
     x.addEventListener('click', function () {  location.href= "https://www.linkedin.com/in/ezequiel-san-pedro-24439b17b/" }) 
 });
+
+
+
 
 var urlPong=document.querySelector('#urlPong')
 
