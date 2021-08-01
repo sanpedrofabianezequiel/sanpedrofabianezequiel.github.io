c35f8bfa8e278bb9546c3f1cd0330f40539eaf78
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2021-08-01 15:24:23 -0300

    Update presupuesto

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ idLinkedin.addEventListener('click',(e) => { location.href= "https://www.linkedi
 
 
 var presupesto= document.querySelector("#presupuesto1")
-presupesto.addEventListener('click',(e) => { location.href= "https://vipe1000.github.io/destiny/"; console.log(presupesto)})
+presupesto.addEventListener('click',(e) => { location.href= "https://sanpedrofabianezequiel.github.io/australia/"; console.log(presupesto)})
+
+var compentencia= document.querySelector("#compentencia")
+compentencia.addEventListener('click',(e) => { location.href= "https://www.codeandvisual.com.au/package/landing-page"; console.log(compentencia)})
 
 var idIconos= document.querySelectorAll(".sPequeña");
 
@@ -11,7 +14,7 @@ idIconos.forEach(   x => {
     x.addEventListener('click', function () {  location.href= "https://www.linkedin.com/in/ezequiel-san-pedro-24439b17b/" }) 
 });
 var whatsaap=document.querySelector("#whatsapp");
-whatsaap.addEventListener('click',(e)=>{  location.href="https://wa.link/ho11zg"})
+whatsaap.addEventListener('click',(e)=>{  location.href="https://wa.link/9mutai"})
 
 
 var git=document.querySelector("#git");
